43b4861e57d3fb59b1428b36f4226e0ded4f2d84
vishnu vivek <vishnuvivek@spericorn.com>
2023-02-08 13:13:18 +0530

    port number changed

--- bin/www.js
+++ bin/www.js
@@ -1,52 +1,50 @@
 import http from 'http';
-import app from '../app.js'
+import app from '../app.js';
 
 const normalizePort = (val) => {
-    const port = parseInt(val, 10);
-  
-    if (Number.isNaN(port)) {
-      return val;
-    }
-  
-    if (port < 0) {
-      logger.error('port must be greater than or equal zero.');
-      process.exit(1);
-    }
-  
-    return port;
-  };
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port < 0) {
+    logger.error('port must be greater than or equal zero.');
+    process.exit(1);
+  }
+
+  return port;
+};
 
 
-
-  const port = normalizePort(process.env.PORT || '5000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 const server = http.createServer(app);
 
 
-
 const errorHandler = (error) => {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-  
-    const address = server.address();
-    const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
-  
-    switch (error.code) {
-      case 'EACCES':
-        console.log(`${bind} requires elevated privileges.`);
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.log(`${bind} is already in use.`);
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-  server.on('error', errorHandler);
+  const address = server.address();
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.log(`${bind} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`${bind} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+server.on('error', errorHandler);
 // make the server listen to requests
 server.on('listening', () => {
   const address = server.address();
